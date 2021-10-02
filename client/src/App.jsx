4123b63b7a2704da8297a3bc36e1d1e7dd8d86cd
Banzak <leosantanabr@gmail.com>
2021-10-02 11:43:24 -0300

    Criando componente para enviar as informações dos produtos para o banco de dados

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import React,{ Fragment } from 'react';
+import './App.css';
+
+//components
+
+import InputProdutos from './components/InputProdutos';
+
+function App() {
+  return (
+  <Fragment>
+    <div className="container">
+    <InputProdutos />
+    </div>
+  </Fragment>
+  );
+};
+
+export default App;
---
+++ client/src/components/InputProdutos.jsx
@@ -0,0 +1,40 @@
+import React, { Fragment, useState } from 'react';
+import './InputProdutos.css';
+
+const InputProdutos = () => {
+
+    const [name, setName] = useState("");
+    const [price, setPrice] = useState();
+    const [inventory, setInventory] = useState();
+
+    const onSubmitForm = async (e) => {
+        e.preventDefault();
+        try {
+            const body = { name, price, inventory };
+            const response = await fetch("http://localhost:5000/produtos",{
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify(body)
+            });
+
+            console.log(response);
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+
+    return (
+        <Fragment>
+            <h1 className="text-center mt-5">Lista de Produtos</h1>
+            <form className="d-flex flex-column mt-5  align-items-center" onSubmit={onSubmitForm}>
+                <input id="name" type="text" className="form-control mt-3 w-25" value={name} onChange={e => setName(e.target.value)} placeholder="Nome"/>
+                <input id="price" type="number" className="form-control mt-3 w-25" value={price} onChange={e => setPrice(e.target.value)} placeholder="Valor do Produto"/>
+                <input id="inventory" type="number" className="form-control mt-3 w-25" value={inventory} onChange={e => setInventory(e.target.value)} placeholder="Quantidade"/>
+
+                <button className="btn btn-success w-10">Adicionar</button>
+            </form>
+        </Fragment>
+    )
+};
+
+export default InputProdutos;
